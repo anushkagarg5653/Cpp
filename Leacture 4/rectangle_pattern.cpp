68e3fb3d1b675869036c185a184d7bf3c0232a6f
anushkagarg5653 <anushkagarg0811@gmail.com>
2021-05-27 11:14:11 +0530

    initial commit

---
+++ Leacture 4/rectangle_pattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row, col,i,j;
+    cout<<"Enter the number of rows and column : ";
+    cin>>row>>col;
+
+    for(i=1;i<=row; i++){
+        for(j=1; j<=col; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
