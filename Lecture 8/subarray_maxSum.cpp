d6abff24f3dfd0891f9f5166d30f18e3957a1219
anushkagarg5653 <anushkagarg0811@gmail.com>
2021-07-08 10:37:21 +0530

    subarrays

---
+++ Lecture 8/subarray_maxSum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    int sum=0;
+    cout<<"Enter the size of an array : ";
+
+    for (int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    int maxSum= INT_MIN;
+    for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            for(int k=i; k<=j; k++){
+                sum +=a[k];
+            }maxSum = max(maxSum, sum);
+        }
+    }cout<<maxSum<<endl;
+    return 0;
+}
