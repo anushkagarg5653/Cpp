a9acafef542ea38fe2f5d3051783632da111ba80
anushkagarg5653 <anushkagarg0811@gmail.com>
2021-12-14 15:03:24 +0530

    print counting numbers

---
+++ dsa cpp/Lecture 8/couting.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void Counting(int n){
+    for(int i=0;i<=n;i++){
+        cout<<i<<endl;
+    }
+};
+int main(){
+    int n;
+    cin>>n;
+    Counting(n);
+
+    return 0;
+}
