a8b6957a32d3e3ce76b93105512741f205eea905
anushkagarg5653 <anushkagarg0811@gmail.com>
2022-01-21 14:11:29 +0530

    Array Practice ||

---
+++ dsa cpp/Lecture 21-Leetcode-Array/rotateArr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void rotate(vector<int>& arr, int k){
+    vector<int> temp(arr.size());
+    for(int i=0;i<arr.size(); i++){
+        temp[(i+k)%arr.size()] = arr[i];
+    }
+    arr = temp;
+}
+
+int main(){
+    
+    return 0;
+}
