c7f6940c2fb51db0051b570befd4e39b62a517ce
anushkagarg5653 <anushkagarg0811@gmail.com>
2022-02-22 09:56:26 +0530

    permutation of a string

---
+++ dsa cpp/Lecture 39 Permutation of String/permutationOfString.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+using namespace std;
+
+private:
+    void solve(vector<int> nums,vector<vector<int>>& ans, int index ){
+        //base case
+        if(index >= nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index], nums[i]);
+            solve(nums,ans,index+1);
+            //backtrack
+            swap(nums[index], nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums,ans,index);
+        return ans;
+    }
+};
