4b04e67dc7e0d31788dba58cac4b8cf427549e93
anushkagarg5653 <58622029+anushkagarg5653@users.noreply.github.com>
2022-07-07 11:35:39 +0530

    Find minimum and maximum element in an array

---
+++ 450 DSA cracker/Strings/MinMax.cpp
@@ -0,0 +1,14 @@
+int max = INT_MIN;
+    int min = INT_MAX;
+  
+    for(int i=0;i<n;i++){
+        if(a[i]>max)
+                max = a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]<min)
+            min = a[i];
+    }
+  
+    return make_pair(min,max);
+   
